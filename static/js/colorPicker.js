33a408f0e1a676dd2b6581a48bb9473c6e1bf087
wym <731162160@qq.com>
2021-11-18 18:33:44 +0800

    updaload code

--- static/js/colorPicker.js
+++ static/js/colorPicker.js
@@ -2,6 +2,7 @@ function ColorPicker(option = {}) {
     this.init(option);
     this.create();
     this.bindEvent();
+    this.drawPanel();
     return EventTarget(this);
 }
 
@@ -12,6 +13,13 @@ ColorPicker.prototype.init = function (option = {}) {
         backgroundColor: "#1d2129"
     }
 
+    this.color = {
+        h: 0,
+        s: "100%",
+        l: "50%",
+        a: 1
+    }
+
     this.option = Object.assign(defaultOpt, option)
     this.$el = document.querySelector(option.el);
 }
@@ -19,31 +27,36 @@ ColorPicker.prototype.init = function (option = {}) {
 ColorPicker.prototype.create = function () {
     const wrapper = createContainer('color-picker-wrapper');
     const container = createContainer('color-picker');
+    let panel = new createContainer('color-picker-panel');
+    panel = this.createCanvas(panel);
     const slider = EventTarget(new Slider('color-picker-cs'));
-    const panel = new createContainer('color-picker-panel');
+    const opacitySlider = EventTarget(new Slider('color-picker-os'));
 
     container.appendChild(panel);
     container.appendChild(slider.$el);
+    container.appendChild(opacitySlider.$el);
     wrapper.appendChild(container);
     document.body.appendChild(wrapper);
 
     this.wrapper = wrapper;
     this.container = container;
-    this.slider = slider;
     this.panel = panel;
+    this.slider = slider;
+    this.opacitySlider = opacitySlider;
     this.$el.innerText = this.option.value;
-
-
 }
 
 ColorPicker.prototype.bindEvent = function () {
-    const {$el, container, slider, panel, wrapper} = this;
+    const {$el, container, slider, panel, wrapper, opacitySlider} = this;
     const _this = this;
     this.elClick($el, container);
 
     slider.$on("change", (val) => {
-        panel.innerText = val / 100
-        this.$emit("change", val);
+        this.$emit("hueChange", val);
+    })
+
+    opacitySlider.$on("change", (val) => {
+        this.$emit("opacityChange", val);
     })
 
     document.addEventListener('mousedown', function (e) {
@@ -71,6 +84,46 @@ ColorPicker.prototype.elClick = function (el, panel) {
         }
         this.wrapper.style.cssText = styleText;
     })
+}
+
+ColorPicker.prototype.createCanvas = function (container) {
+    const canvas = this.canvas = document.createElement('canvas');
+    const cursor = document.createElement('i');
+    cursor.className = 'move'
+    container.appendChild(canvas);
+    container.appendChild(cursor);
+    return container;
+}
+
+ColorPicker.prototype.drawPanel = function () {
+    const {canvas, color} = this;
+    const ctx = canvas.getContext("2d");
+    const linearGradient = ctx.createLinearGradient(0, 0, canvas.width, 1);
+    linearGradient.addColorStop(0, "#fff");
+    linearGradient.addColorStop(1, `hsl(${color.h}, 100%, 50%)`);
+    ctx.fillStyle = linearGradient;
+    ctx.fillRect(0, 0, canvas.width, 1);
+
+    //获取水平方向上的像素点
+    let pixelData = ctx.getImageData(0, 0, canvas.width - 1, 1).data;
+    console.log(pixelData);
+    //遍历数组
+    for (let i = 0; i < pixelData.length; i += 4) {
+        // 获取数据
+        let data = {
+            r: pixelData[i],
+            g: pixelData[i + 1],
+            b: pixelData[i + 2],
+            a: pixelData[i + 3]
+        };
+        //创建线性渐变
+        let gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+        gradient.addColorStop(0, `rgb(${data.r}, ${data.g}, ${data.b})`);
+        gradient.addColorStop(1, `rgb(0, 0, 0)`);
+        //画线
+        ctx.fillStyle = gradient;
+        ctx.fillRect(i / 4, 0, i / 4, canvas.height - 1);
+    }
 }
 
 function Slider(className) {
@@ -131,7 +184,7 @@ function createContainer(className) {
 // 观察者模式
 function EventTarget(target) {
     const taskQueue = {}
-    const prototype = target.prototype || target.__proto__
+    const prototype = target
     prototype.$on = function (eventName, handler) {
         !taskQueue[eventName] && (taskQueue[eventName] = []);
         taskQueue[eventName].push(handler);
@@ -139,7 +192,7 @@ function EventTarget(target) {
 
     prototype.$emit = function (eventName, ...args) {
         if (taskQueue[eventName]) {
-            taskQueue[eventName].forEach(fn => fn(...args));
+            taskQueue[eventName].forEach(fn => fn.apply(this, args));
         }
     }
 
